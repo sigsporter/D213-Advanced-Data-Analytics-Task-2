f7d92f44f4e2a0811a52f103c00a3707ace6c168
Stephen Porter <sigsporter@gmail.com>
2023-01-31 19:50:11 -0600

    Beginning review cleanup - remove punctuation, stopwords

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import re
+
+from nltk.corpus import stopwords
 
 colnames = ['review', 'rating']
 yelp = pd.read_csv('C:\\WGU\\D213 Advanced Data Analytics\\Task 2\\yelp_labelled.txt',
@@ -13,13 +16,26 @@ pd.set_option('max_colwidth', 30)
 pd.set_option('display.max_columns', 10)
 
 print(df.info())
-print(df.head(10))
+print(df.head(10), '\n')
 
 
 df['word_count'] = [len(x.split()) for x in df['review']]
 df['char_count'] = df['review'].apply(len)
 print(df)
-print(df['rating'].value_counts())
+print(df['rating'].value_counts(), '\n')
+print("Max word count: ", max(len(x.split()) for x in df['review']), '\n')
 
 # Review Cleanup
 df['review'] = df['review'].astype(str).str.lower()
+df['review_clean'] = df['review'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
+print(df['review_clean'], '\n')
+
+stopwords = set(stopwords.words('english'))
+
+df['no_stop'] = df['review_clean'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
+print(df['no_stop'].head(10), '\n')
+
+max_words = max(len(x.split()) for x in df['no_stop'])
+print("Max word count: ", max_words)
+
+
