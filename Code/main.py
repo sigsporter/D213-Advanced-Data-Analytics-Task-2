99b0b69189ce6ccad10cccceb2a51b457ad9b5ae
Stephen Porter <sigsporter@gmail.com>
2023-02-07 21:12:27 -0600

    Cleaned, Tokenized, Vectorized, Padded. Initial Model Accuracy 96%. Looking good - check for overfitting. Clean up commented code. Nearly complete.

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,17 @@
 # Warnings
 import warnings
+warnings.filterwarnings('ignore')
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
+import re
+
+import numpy as np
 import stopwords as stopwords
+from keras import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout, SpatialDropout1D
+from keras.utils import pad_sequences
+from sklearn.model_selection import train_test_split
 
 warnings.filterwarnings('ignore')
 
@@ -99,31 +109,44 @@ print(dfPN['rating'].value_counts())
 print(dfPN.shape)
 ################################################## REVIEW TEXT HANDLING ################################################
 # Convert to lowercase and remove punctuation
-dfPN.reviewText = dfPN.reviewText.str.lower()
+def isEnglish(dataframe):
+    notAsciiCount = 0
+    for x in dataframe:
+        if not x.isascii():
+            notAsciiCount += 1
+    return notAsciiCount
+
+print("Count of reviews with non-English characters: ", isEnglish(dfPN['reviewText']), '\n')
+
+dfPN['reviewText'] = dfPN['reviewText'].apply(lambda x: ''.join(char for char in x if ord(char) < 128))
+dfPN['reviewText'] = dfPN['reviewText'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
+dfPN['reviewText'] = dfPN['reviewText'].str.lower()
+
 
 cleaned = RegexpTokenizer('\w+')
-dfPN['tok_res'] = dfPN['reviewText'].apply(cleaned.tokenize)
+dfPN['tok'] = dfPN['reviewText'].apply(cleaned.tokenize)
 print(dfPN.head(10))
 
-stopwords = set(stopwords.words('english'))
 
-def remove_stopwords(text):
-    output = [i for i in text if i not in stopwords]
-    return output
+# stopwords = set(stopwords.words('english'))
+#
+# def remove_stopwords(text):
+#     output = [i for i in text if i not in stopwords]
+#     return output
+#
+# dfPN['no_stop'] = dfPN['tok_res'].apply(lambda x: remove_stopwords(x))
+# print("Stopwords Removed:")
+# print(dfPN['no_stop'])
 
-dfPN['no_stop'] = dfPN['tok_res'].apply(lambda x: remove_stopwords(x))
-print("Stopwords Removed:")
-print(dfPN['no_stop'])
-
-wordnet_lemmatizer = WordNetLemmatizer()
-
-def lemmatizer(text):
-    lemm_text = [wordnet_lemmatizer.lemmatize(word) for word in text]
-    return lemm_text
-
-dfPN['lem'] = dfPN['no_stop'].apply(lambda x: lemmatizer(x))
-print("Stopwords Removed & Lemmatized")
-print(dfPN['lem'])
+# wordnet_lemmatizer = WordNetLemmatizer()
+#
+# def lemmatizer(text):
+#     lemm_text = [wordnet_lemmatizer.lemmatize(word) for word in text]
+#     return lemm_text
+#
+# dfPN['lem'] = dfPN['no_stop'].apply(lambda x: lemmatizer(x))
+# print("Stopwords Removed & Lemmatized")
+# print(dfPN['lem'])
 
 # Wordcloud stuff - keep for later
 
@@ -136,20 +159,20 @@ print(dfPN['lem'])
 
 ################################################## REVIEW TEXT WORDCLOUDS ##############################################
 
-# Wordcloud for all reviews
-def lem_string(list):
-    string = ' '.join(list)
-    return string
-
-dfPN['lem_string'] = dfPN['lem'].apply(lambda x: lem_string(x))
-print(dfPN['lem_string'])
-
-text = "".join(x for x in dfPN['lem_string'])
-wordcloud = WordCloud(stopwords=stopwords).generate(text)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.savefig("wordcloud1.png")
-plt.show()
+# # Wordcloud for all reviews
+# def lem_string(list):
+#     string = ' '.join(list)
+#     return string
+#
+# dfPN['lem_string'] = dfPN['lem'].apply(lambda x: lem_string(x))
+# print(dfPN['lem_string'])
+#
+# text = "".join(x for x in dfPN['lem_string'])
+# wordcloud = WordCloud(stopwords=stopwords).generate(text)
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+# plt.savefig("wordcloud1.png")
+# plt.show()
 
 # # Wordcloud for positive reviews
 # dfPos = dfPN[dfPN['rating'] == 1]
@@ -169,9 +192,61 @@ plt.show()
 # plt.savefig("wordcloudNeg.png")
 # plt.show()
 
+################################################## SETTING PARAMETERS ##################################################
+all_words = []
+for x in dfPN['tok']:
+    for i in x:
+        all_words.append(i)
+
+unique_words = set(all_words)
+max_words = len(unique_words)
+print("Count of unique words:", max_words)
+# End borrowed code
+
+longest_review = 0
+for x in dfPN['tok']:
+    if len(x) > longest_review:
+        longest_review = len(x)
+
+print("The review with the most words has", longest_review, "words.\n")
+
+################################################## VECTORIZING & PADDING ###############################################
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(dfPN['tok'])
+seq = tokenizer.texts_to_sequences(dfPN['tok'])
+print("Embedded sentences:")
+print(seq[0:10], '\n')
+review_vectors = pad_sequences(seq, padding='post', maxlen=longest_review)
+print("Padded sentences:")
+print(review_vectors[0:10], '\n')
+
 ################################################## TRAIN/TEST SPLIT ####################################################
+ratings = list(dfPN['rating'])
+
+reviews_train, reviews_test, ratings_train, ratings_test = train_test_split(review_vectors, ratings, test_size=0.2,
+                                                                            random_state=22)
+
+print("Reviews Train:\n", reviews_train[0:10])
+print("Reviews Test:\n", reviews_test[0:10])
+print("Ratings Train:\n", ratings_train[0:10])
+print("Ratings Test:\n", ratings_test[0:10])
+
+# np.savetxt('C:\\WGU\\D213 Advanced Data Analytics\\Task 2\\review_vextors_2.csv', review_vectors, delimiter=', ', fmt='% s')
+# np.savetxt('C:\\WGU\\D213 Advanced Data Analytics\\Task 2\\ratings_2.csv', ratings, delimiter=', ', fmt='% s')
 
 
 ################################################## MODELING & PREDICTING ###############################################
+embedding_vector_length = 32
+model = Sequential()
+model.add(Embedding(input_dim=max_words, output_dim=embedding_vector_length, input_length=longest_review))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(units=128))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
 
-
+# loss1, acc1, mse1 =
+print(model.evaluate(reviews_test, np.array(ratings_test)))
+# history = model.fit(reviews_train, np.array(ratings_train), validation_split=0.2, epochs=5, batch_size=32,
+#                     validation_data=(reviews_test, np.array(ratings_test)))
