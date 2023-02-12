fd7f8da7aa2d29e4faac8194c23658d484ee31be
Stephen Porter <sigsporter@gmail.com>
2023-02-11 18:32:05 -0600

    Improved model accuracy by reducing input length - longest_review = 686 is a massive outlier. Revisit Histogram

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,5 @@
 # Imports
+import math
 import warnings
 warnings.filterwarnings('ignore')
 import os
@@ -6,11 +7,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 import pandas as pd
 import re
+import nltk
 import matplotlib.pyplot as plt
 import numpy as np
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from textblob import Word
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from wordcloud import WordCloud
@@ -49,63 +52,73 @@ df['review'] = df['review'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
 df['review'] = df['review'].str.lower()
 
 # print("New count of reviews with non-English characters: ", isEnglish(df['review']), '\n')
+print("Value counts of ratings:")
+print(df['rating'].value_counts(), '\n')
 
-# stopwords = set(stopwords.words('english'))
-# df['no_stop'] = df['review'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
-# print(df['no_stop'].head(10), '\n')
+stopwords = set(stopwords.words('english'))
+df['review'] = df['review'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
+print(df['review'].head(10), '\n')
 
-cleaned = RegexpTokenizer('\w+')
-df['tok'] = df['review'].apply(cleaned.tokenize)
-print(df['tok'].head(10), '\n')
+
 
 # wordnet_lemmatizer = WordNetLemmatizer()
 # def lemmatizer(text):
 #     lemm_text = [wordnet_lemmatizer.lemmatize(word) for word in text]
 #     return lemm_text
-#
-# df['lem'] = df['tok'].apply(lambda x: lemmatizer(x))
-# print("Lemmatized")
-# print(df['lem'].head(10), '\n')
+
+df['review'] = df['review'].apply(lambda x: ' '.join([Word(x).lemmatize() for x in x.split()]))
+print("Lemmatized")
+print(df['review'].head(10), '\n')
+
+# cleaned = RegexpTokenizer('\w+')
+# df['review'] = df['review'].apply(cleaned.tokenize)
+# print(df['review'].head(10), '\n')
 
 # Borrowed Code Author: Usman Malik
 # URL: https://stackabuse.com/python-for-nlp-word-embeddings-for-deep-learning-in-keras/
 all_words = []
-for x in df['tok']:
-    for i in x:
-        all_words.append(i)
+for x in df['review']:
+    tokenize_word = word_tokenize(x)
+    for word in tokenize_word:
+        all_words.append(word)
 
 unique_words = set(all_words)
 max_words = len(unique_words)
 print("Count of unique words:", max_words)
 # End borrowed code
 
-longest_review = 0
-for x in df['tok']:
-    if len(x) > longest_review:
-        longest_review = len(x)
-
+df['length'] = df['review'].str.split().str.len()
+longest_review = df['length'].max()
 print("The review with the most words has", longest_review, "words.\n")
+
+mean = math.ceil(df['length'].mean())
+print(mean)
 
 # padding & vectorizing
 tokenizer = Tokenizer(num_words=max_words)
-tokenizer.fit_on_texts(df['tok'])
-seq = tokenizer.texts_to_sequences(df['tok'])
+tokenizer.fit_on_texts(df['review'])
+seq = tokenizer.texts_to_sequences(df['review'])
 print("Embedded sentences:")
 print(seq[0:10], '\n')
-review_vectors = pad_sequences(seq, padding='post', maxlen=longest_review)
+review_vectors = pad_sequences(seq, padding='post', maxlen=mean)
 print("Padded sentences:")
 print(review_vectors[0:10], '\n')
+
 
 # Train/Test Split
 ratings = list(df['rating'])
 
-reviews_train, reviews_test, ratings_train, ratings_test = train_test_split(review_vectors, ratings, test_size=0.2,
+reviews_train, reviews_test, ratings_train, ratings_test = train_test_split(review_vectors, ratings, test_size=0.3,
                                                                             random_state=22)
 
 print("Reviews Train:\n", reviews_train[0:10])
+print("Length:", len(reviews_train))
 print("Reviews Test:\n", reviews_test[0:10])
+print("Length:", len(reviews_test))
 print("Ratings Train:\n", ratings_train[0:10])
+print("Length:", len(ratings_train))
 print("Ratings Test:\n", ratings_test[0:10])
+print("Length:", len(ratings_test))
 
 # np.savetxt('C:\\WGU\\D213 Advanced Data Analytics\\Task 2\\review_vextors_2.csv', review_vectors, delimiter=', ', fmt='% s')
 # np.savetxt('C:\\WGU\\D213 Advanced Data Analytics\\Task 2\\ratings_2.csv', ratings, delimiter=', ', fmt='% s')
@@ -113,18 +126,19 @@ print("Ratings Test:\n", ratings_test[0:10])
 # Model
 embedding_vector_length = 32
 model = Sequential()
-model.add(Embedding(input_dim=max_words, output_dim=embedding_vector_length, input_length=longest_review))
-# model.add(SpatialDropout1D(0.2))
-model.add(LSTM(units=128))
-# model.add(Dropout(0.2))
+model.add(Embedding(input_dim=max_words, output_dim=embedding_vector_length, input_length=mean))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(units=128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='LeakyReLU'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-# loss1, acc1, mse1 =
-print(model.evaluate(reviews_test, np.array(ratings_test)))
-# history = model.fit(reviews_train, np.array(ratings_train), validation_split=0.2, epochs=5, batch_size=32,
-#                     validation_data=(reviews_test, np.array(ratings_test)))
+# # loss1, acc1, mse1 =
+# # print(model.evaluate(reviews_test, np.array(ratings_test)))
+history = model.fit(reviews_train, np.array(ratings_train), epochs=5,
+                    validation_data=(reviews_test, np.array(ratings_test)))
 
 
 '''
